870985db6857c24bd035d303b4dee6151b26bd4b
unknown <jonaelaziz@outlook.com>
2025-08-06 20:02:16 -0600

    Se corrige la exportacion del servidor para Vercel

--- server.js
+++ server.js
@@ -45,7 +45,4 @@ io.on('connection', socket => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
+module.exports = app;
